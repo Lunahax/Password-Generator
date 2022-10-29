65917d2abbe6e61e7414f5afaffa17546a211dce
Lunahax <89605624+Lunahax@users.noreply.github.com>
2022-10-29 23:23:25 +0200

    Create Source Code.js

---
+++ Source Code.js
@@ -0,0 +1,1 @@
+function g(length) {var result='';var characters='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?&_-#';var charactersLength=characters.length;for ( var i = 0; i < length; i++ ) {result+=characters.charAt(Math.floor(Math.random()*charactersLength));}return result;};console.log(g(16));//Made by Lunahax
